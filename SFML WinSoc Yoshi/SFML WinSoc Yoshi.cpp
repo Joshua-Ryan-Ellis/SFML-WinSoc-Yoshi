a510bdef2cf79e36041fa5a27f9d8f42ff6a4651
JoshuaRyanEllis <arkodegames.jellis@gmail.com>
2021-03-18 10:50:47 +0000

    Add project files.

---
+++ SFML WinSoc Yoshi/SFML WinSoc Yoshi.cpp
@@ -0,0 +1,42 @@
+// SFML WinSoc Yoshi.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <SFML\Graphics.hpp>
+#include <SFML\System.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(1000, 600), "Yoshi", sf::Style::Close | sf::Style::Titlebar);
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+			//if(event.type == sf::Event::)
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
